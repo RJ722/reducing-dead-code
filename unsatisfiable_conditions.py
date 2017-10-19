8dde2036591ac0d1c1c9fba8d03172b9e600800b
RJ722 <rahul722j@gmail.com>
2017-10-19 22:28:22 +0530

    Finish Section2.2: Using vulture

---
+++ unsatisfiable_conditions.py
@@ -0,0 +1,6 @@
+if 0:
+    print("Heh!")
+if False:
+    print("Haha!")
+if not True:
+    print("Not true")
---
+++ unused_code.py
@@ -0,0 +1,5 @@
+def hello():
+    return "Hello"
+
+if __name__ == '__main__':
+    print("Hi!")
